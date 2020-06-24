6fc9ad92b9fbb60c0ee5ee0b51a6518876f5bc8c
omrobbie <putu.roby@yahoo.com>
2020-06-24 13:36:45 +0800

    Initial Commit

---
+++ CineMix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CineMix
+//
+//  Created by omrobbie on 24/06/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
